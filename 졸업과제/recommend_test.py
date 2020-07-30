7691209c352575d03d8e54e8f3e0b7ad9ef2265e
coding_anham <xkzl0327@gmail.com>
2020-07-30 15:53:42 +0900

    recommend_test.py 추가

---
+++ 졸업과제/recommend_test.py
@@ -0,0 +1,22 @@
+import pickle
+from hyperparams import *
+import numpy as np
+
+RFC_A = pickle.load(open(PIC_PATH["Voting"]["A"], 'rb'))
+RFC_B = pickle.load(open(PIC_PATH["Voting"]["B"], 'rb'))
+
+
+for i in range(10):
+    Train_A = np.load(NPZ_PATH["rand"][i])
+    Train_P = np.load(NPZ_PATH["protein"])
+
+    X_A = Train_A['XA']
+    X_P = Train_P['XP']
+
+    Train = np.concatenate((X_P, X_A), axis=-1)
+
+    pdA = RFC_A.predict(Train)
+    pdB = RFC_B.predict(Train)
+
+    print("A {}".format(pdA))
+    print("B {}".format(pdB))
