3033583177f16ef969579efc7ed86d438ca7b233
coding_anham <chan_az@pusan.ac.kr>
2020-07-30 17:29:53 +0900

    디테일 수정

--- 졸업과제/recommend_test.py
+++ 졸업과제/recommend_test.py
@@ -2,8 +2,7 @@ import pickle
 from hyperparams import *
 import numpy as np
 
-RFC_A = pickle.load(open(PIC_PATH["mix_randomForests"]["A"], 'rb'))
-RFC_B = pickle.load(open(PIC_PATH["mix_randomForests"]["B"], 'rb'))
+RFC_A = pickle.load(open(PIC_PATH["mix_randomForests"], 'rb'))
 
 
 for i in range(10):
@@ -16,8 +15,5 @@ for i in range(10):
     Train = np.concatenate((X_P, X_A), axis=-1)
 
     pdA = RFC_A.predict(Train)
-    pdB = RFC_B.predict(Train)
 
-    #print("A {}".format(pdA))
-    #print("B {}".format(pdB))
-#test
+    print("A {}".format(pdA))
